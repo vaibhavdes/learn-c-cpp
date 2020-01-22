725d4835a763de53f2eca19c372136dc36eb422a
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2020-01-22 23:13:15 +0530

    Create 4.Enum.c
    Implement Enumeration in C

---
+++ 2.String_Conditional_Enum/4.Enum.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+enum color
+{
+	RED, BLACK, GREEN=5, YELLOW
+};
+int main(void)
+{
+	// enum color is user defined data type , c variable of enum color
+	enum color c;
+	int no;
+
+	printf("\n sizeof c=%d", sizeof(c));
+	printf("\n sizeof enum color=%d", sizeof(enum color));
+
+	printf("\n 0. Red 1. Black 5. Green 6. Yellow :: ");
+	printf("\n Enter your choice :: ");
+	//scanf("%d", &c); // way 1 scan enum variable c
+
+	scanf("%d", &no);
+	//c= (enum color )no; // way 2 scan int typecast with enum
+
+	// way 3 // scan int and assign value using switch case
+
+	switch(no)
+	{
+		default: printf("\n invalid color ::");exit(0);
+		case 0: c= RED;break;
+		case 1: c= BLACK; break;
+		case 5: c= GREEN;break;
+		case 6: c= YELLOW;break;
+	}
+
+	// display data using enum
+	switch(c)
+	{
+		case RED: printf("\n red color "); break;
+		case BLACK: printf("\n black color "); break;
+		case GREEN: printf("\n green color "); break;
+		case YELLOW: printf("\n Yellow color "); break;
+		default: printf("\n invalid color");
+
+	}
+
+	return 0;
+}
+
+
+
